67ed4177f63b3a9dc7bbf56fbca58e6cd4e0a618
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-05-27 22:18:53 -0400

    Styling the video player

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -17,13 +18,16 @@ class App extends React.Component {
     };
 
     onVideoSelect = video => {
-        
+        this.setState({ selectedVideo: video });
     }
+
+
     //passing a prop called video from the state to VideoList
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         )
