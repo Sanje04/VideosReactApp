681ff613bc39bfa2a9dd4637bed6842226e13f34
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-05-27 22:58:30 -0400

    Video Project Complete

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
 
     return (
         <div onClick={()=> onVideoSelect(video)} className="video-item item">
-            <img classname="ui image" src={video.snippet.thumbnails.medium.url} />
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
             </div>
